4ff9b84b33178d3be916974feb1d776f9e7b8551
ksripath <ksripath@tibco.com>
2018-12-04 16:07:05 -0800

    Accept request body

--- app.js
+++ app.js
@@ -1,39 +1,34 @@
 var url = `https://tcisf-mapper.flogo.cloud.tibco.com/api`;
-
+var body  = '{}';
 function callAPI(){
     return fetch(url, {
         mode: 'cors',
         method: 'POST',
         header: new Headers({
-            'Access-Control-Allow-Origin':'*',
+            // 'Access-Control-Allow-Origin':'*',
             'Content-Type': 'application/json'
         }),
         timeout: 30000,
-        body: JSON.stringify({ Source: [
-          {
-              "type_length": 240,
-              "type": "STRING",
-              "name": "LastName",
-              "label": "Last Name"
-          }
-      ], Target: [
-          {
-              "type": "string",
-              "label": "Company Name",
-              "type_length": 255,
-              "name": "company"
-          }
-      ] })
-      })
-      .then(response => {
-        console.log(response);
-        return response.json();
-      });
+        body: JSON.stringify(body)
+    })
+    .then(response => {
+    console.log(response);
+    return response.json();
+    });
   }
   
   function callMatcher(){
     var strUrl = document.querySelector("#url").value.trim();
-    url = strUrl!=''?strUrl:url;
+    var strBody = JSON.parse(document.querySelector("#request_body").value.trim());
+    if(!strUrl){
+        alert('Please enter url');
+        return;
+    }else if(!strBody){
+        alert('Please enter POST Body');
+    }else{
+        url = strUrl;
+        body = strBody;
+    }
     callAPI().then(data=>{
         document.querySelector("#response").innerHTML = '<div style="margin:20px"><h3>Response:</h3></div><textarea style="width:800px;height:200px;margin:20px;">'+JSON.stringify(data)+'</textarea>';
       }).catch(error => {
